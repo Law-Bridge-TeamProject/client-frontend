0a9248b118286ce39d5bd0e663203c4fa243388d
khulan <khlnua21@gmail.com>
2025-06-22 18:52:29 +0800

    clerk and graphql

--- src/providers/ApolloWrapper.tsx
+++ src/providers/ApolloWrapper.tsx
@@ -3,28 +3,30 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import { ReactNode } from "react";
+import { useAuth } from "@clerk/nextjs";
 
 const httpLink = createHttpLink({
   uri: process.env.BACKEND_URL || "http://localhost:4000/api/graphql",
 });
 
 export const ApolloWrapper = ({ children }: { children: ReactNode }) => {
-    // const {getToken} = useAuth()
-  const authLink = setContext(async(_, { headers }) => {
 
-    // const clerkToken = localStorage.getItem("token");
+    const {getToken} = useAuth()
+
+  const authLink = setContext(async(_, { headers }) =>{
+    const clerkToken = await getToken();
 
     return {
       headers: {
-        ...headers,
-        // authorization: clerkToken ? `Bearer ${clerkToken}` : "",
-      },
-    };
+        ...headers, 
+        Authorization : clerkToken,
+      }
+    }
   });
 
   const client = new ApolloClient({
-    cache: new InMemoryCache(),
     link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
   });
 
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
