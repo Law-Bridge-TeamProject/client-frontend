96254fc3314acea4fd91f060317e5e02ce19f365
25LP5095 25LP5095 <25LP5095@admins-iMac.local>
2025-06-23 13:47:49 +0800

    aa

---
+++ src/app/chat-room/lib/apollo-client.ts
@@ -0,0 +1,38 @@
+// lib/apollo-client.ts
+import { ApolloClient, InMemoryCache, split, HttpLink } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/api/graphql",
+});
+
+const wsLink =
+  typeof window !== "undefined"
+    ? new GraphQLWsLink(
+        createClient({
+          url: "ws://localhost:4001/graphql",
+        })
+      )
+    : null;
+
+const splitLink =
+  typeof window !== "undefined" && wsLink
+    ? split(
+        ({ query }) => {
+          const definition = getMainDefinition(query);
+          return (
+            definition.kind === "OperationDefinition" &&
+            definition.operation === "subscription"
+          );
+        },
+        wsLink,
+        httpLink
+      )
+    : httpLink;
+
+export const apolloClient = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+});
