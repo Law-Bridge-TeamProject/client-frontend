a8cd05c020d653bd2eca658582a1ae1f7c95f3e9
24LP0723 24LP0723 <24LP0723@admins-iMac.local>
2025-06-25 12:52:19 +0800

    Usukhuu

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    R2_ACCESS_KEY_ID: process.env.R2_ACCESS_KEY_ID,
+    R2_SECRET_ACCESS_KEY: process.env.R2_SECRET_ACCESS_KEY,
+    R2_BUCKET_NAME: process.env.R2_BUCKET_NAME,
+    R2_ENDPOINT: process.env.R2_ENDPOINT,
+    R2_PUBLIC_DOMAIN: process.env.R2_PUBLIC_DOMAIN,
+  },
 };
 
 export default nextConfig;
---
+++ src/app/Usukhuu/components/Avatar.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { useUploadAvatar } from "../hooks/useUploadAvatar";
+import { ZodErrors } from "./ZodError";
+import type { FieldErrors, UseFormSetValue } from "react-hook-form";
+import type { FormData } from "@/app/Usukhuu/page";
+
+type Props = {
+  errors: FieldErrors<FormData>;
+  setValue: UseFormSetValue<FormData>;
+};
+
+const Avatar = ({ errors, setValue }: Props) => {
+  const handleUploadSuccess = (url: string) => {
+    setValue("avatar", url);
+  };
+  const {
+    fileInputRef,
+    previewLink,
+    uploading,
+    isDragging,
+    openBrowse,
+    handleFileSelect,
+    handleDrop,
+    deleteImage,
+    setIsDragging,
+  } = useUploadAvatar({ onUpload: handleUploadSuccess });
+
+  return (
+    <div className="grid grid-cols-2">
+      <div>
+        <label htmlFor="profileImage" className="block text-sm font-medium mb-1">
+          Нүүр зураг оруулах
+        </label>
+        {previewLink && (
+          <button
+            type="button"
+            onClick={deleteImage}
+            className="mt-2 text-sm text-red-500 hover:underline cursor-pointer"
+          >
+            Зураг устгах
+          </button>
+        )}
+        {uploading && <div className="text-sm text-blue-500 mt-2">Илгээж байна...</div>}
+      </div>
+
+      <Input
+        id="profileImage"
+        type="file"
+        accept="image/*"
+        ref={fileInputRef}
+        onChange={handleFileSelect}
+        style={{ display: "none" }}
+      />
+      <div
+        className={`flex items-center justify-center bg-orange-200 size-full max-h-50 rounded-md border-dashed border-2 mb-2 cursor-pointer ${
+          isDragging ? "border-blue-500 bg-blue-50" : "border-gray-400"
+        }`}
+        onClick={openBrowse}
+        onDrop={handleDrop}
+        onDragOver={(e) => {
+          e.preventDefault();
+          setIsDragging(true);
+        }}
+        onDragLeave={() => setIsDragging(false)}
+      >
+        {previewLink ? (
+          <img src={previewLink} alt="" className="h-full" />
+        ) : (
+          <span className="text-gray-500">Click or drag an image here</span>
+        )}
+      </div>
+      <ZodErrors error={errors.avatar?.message ? [errors.avatar.message] : undefined} />
+    </div>
+  );
+};
+
+export default Avatar;
